92ef50b64964593762799fced8e500f6e1788ff6
Gerardo Santos <gesantos.dwfsc16@bootcamp.ucamp.io>
2023-04-01 10:24:31 -0700

    Si

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,1 +1,16 @@
-console.log ("Hola")
+const saludar = (nombre, callback) => {
+    let nombreMayusculas = nombre.toUpperCase()
+
+setTimeout(() => {
+    callback(nombreMayusculas);
+    }, 4000);
+};
+
+const imprimeNombre =(nombreEnMayusculas) => {
+    console.log(nombreEnMayusculas)
+};
+
+saludar("gerardo", imprimeNombre);
+
+console.log("final");
+// imprimeNombre("gerardo")
