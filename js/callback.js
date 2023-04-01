c3d7125c2c71aa222b5f6f2b5490f0a1478ae082
Gerardo Santos <gesantos.dwfsc16@bootcamp.ucamp.io>
2023-04-01 09:48:14 -0700

    otra vez

---
+++ js/callback.js
@@ -0,0 +1,23 @@
+//console.log("Hola");
+//console.log("Como estas?");
+//console.log("Adios");
+
+//const saludar = () => {
+//    console.log ("hola");
+//};
+
+//const preguntar = (callback) => {
+//    setTimeout(() => {
+//        console.log("como estas?");
+//        callback();
+//    }, 3000);
+//};
+
+//const despedir = () => {
+//    console.log ("adios");
+//};
+
+//saludar();
+//preguntar(despedir);
+// despedir();
+
